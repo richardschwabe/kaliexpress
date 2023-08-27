5a0f961b9f3887755856ba120afd502d49f58340
Richard Schwabe <63605187+richardschwabe@users.noreply.github.com>
2023-08-28 02:05:51 +0300

    updated docker related

--- roles/docker/files/add-docker-apt.sh
+++ roles/docker/files/add-docker-apt.sh
@@ -3,6 +3,6 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  "deb arch=amd64" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  bookworm stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
