16af56fcd6abf366c9b8f21ce62aea5f3144ec7a
Richard Schwabe <63605187+richardschwabe@users.noreply.github.com>
2023-08-28 02:28:04 +0300

    gpd --yes

--- roles/docker/files/add-docker-apt.sh
+++ roles/docker/files/add-docker-apt.sh
@@ -1,5 +1,5 @@
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
