457e50ae439d27c9576b322ae735352a2eced700
Richard Schwabe <63605187+richardschwabe@users.noreply.github.com>
2023-08-28 02:07:18 +0300

    fix quote error

--- roles/docker/files/add-docker-apt.sh
+++ roles/docker/files/add-docker-apt.sh
@@ -3,6 +3,6 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
-  "deb arch=amd64" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "deb arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   bookworm stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
