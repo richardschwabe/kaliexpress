1ff61f0a6f80d55ff9d8f09cf50f52cf926d8fd7
Richard Schwabe <63605187+richardschwabe@users.noreply.github.com>
2023-08-28 14:18:04 +0300

    added change bg script

---
+++ roles/base/files/change-bg.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitorVirtual1/workspace0/image-style   --set 0
+xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitorVirtual1/workspace0/color-style --set 0
